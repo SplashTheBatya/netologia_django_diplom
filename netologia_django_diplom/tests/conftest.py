8fd7f47770a8e0e54f7ba8887313752198ca42db
Kakolay Nimalov <47940537+SplashTheBatya@users.noreply.github.com>
2021-05-07 23:34:34 +0300

    Started tests

--- netologia_django_diplom/tests/conftest.py
+++ netologia_django_diplom/tests/conftest.py
@@ -1,11 +1,27 @@
 import pytest
+from django.contrib.auth.models import User
 from model_bakery import baker
 from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
 
 
 @pytest.fixture
 def api_client():
     return APIClient()
+
+
+@pytest.fixture
+def get_or_create_admin_token(db, admin_user):
+    user = admin_user
+    token, _ = Token.objects.get_or_create(user=user)
+    return token
+
+
+@pytest.fixture
+def get_or_create_user_token(db, api_client):
+    user = User.objects.create()
+    token, _ = Token.objects.get_or_create(user=user)
+    return token
 
 
 @pytest.fixture
@@ -16,3 +32,9 @@ def product_factory():
     return factory
 
 
+@pytest.fixture
+def review_factory():
+    def factory(**kwargs):
+        return baker.make('marketplace.Review', **kwargs)
+
+    return factory
--- netologia_django_diplom/tests/marketplace/test_marketplace_api.py
+++ netologia_django_diplom/tests/marketplace/test_marketplace_api.py
@@ -49,18 +49,18 @@ def test_sort_products_by_id(api_client, product_factory):
 
     # act
     resp = api_client.get(url)
-    sorted_coursers = {product_1.id, product_2.id, product_3.id}
+    sorted_products = {product_1.id, product_2.id, product_3.id}
 
     # assert
     assert resp.status_code == HTTP_200_OK
 
     resp_json = resp.json()
     resp_ids = {r["id"] for r in resp_json}
-    assert resp_ids == sorted_coursers
+    assert resp_ids == sorted_products
 
 
 @pytest.mark.django_db
-def test_sort_courses_by_name(api_client, product_factory):
+def test_sort_products_by_name(api_client, product_factory):
     # arrange
     url = reverse('product-list')
     product1 = product_factory(name='Computer')
@@ -77,3 +77,182 @@ def test_sort_courses_by_name(api_client, product_factory):
     resp_json = resp.json()
     resp_ids = {r["name"] for r in resp_json}
     assert resp_ids == sorted_products
+
+
+@pytest.mark.django_db
+def test_admin_post_product(api_client, get_or_create_admin_token):
+    # arrange
+    admin_token = get_or_create_admin_token
+    admin = api_client
+
+    admin.credentials(HTTP_AUTHORIZATION='Token ' + admin_token.key)
+    url = reverse('product-list')
+    payload = {
+        "name": "Product5",
+        "description": "Some product bla bla bla",
+        "price": 5000
+    }
+
+    # act
+    product_count = Product.objects.count()
+    resp = admin.post(url, payload, format="json")
+
+    # assert
+    assert resp.status_code == HTTP_201_CREATED
+    assert Product.objects.count() > product_count
+
+
+@pytest.mark.django_db
+def test_user_post_product(api_client, get_or_create_user_token):
+    # arrange
+    user = api_client
+    user_token = get_or_create_user_token
+
+    user.credentials(HTTP_AUTHORIZATION='Token ' + user_token.key)
+    url = reverse('product-list')
+    payload = {
+        "name": "Product6",
+        "description": "Some product bla bla bla",
+        "price": 50000
+    }
+
+    # act
+    product_count = Product.objects.count()
+    resp = user.post(url, payload, format="json")
+
+    # assert
+    assert resp.status_code == HTTP_403_FORBIDDEN
+    assert product_count == Product.objects.count()
+
+
+@pytest.mark.django_db
+def test_admin_patch_product(api_client, get_or_create_admin_token, product_factory):
+    # arrange
+    product = product_factory()
+    admin = api_client
+    admin_token = get_or_create_admin_token
+
+    admin.credentials(HTTP_AUTHORIZATION='Token ' + admin_token.key)
+    url = reverse('product-detail', args=(product.id,))
+    payload = {
+        "name": "Amazing Product"
+    }
+
+    # act
+    resp = api_client.patch(url, payload, format="json")
+    resp_json = resp.json()
+    expected_name = payload["name"]
+
+    # assert
+    assert resp.status_code == HTTP_200_OK
+    assert resp_json["name"] == expected_name
+
+
+@pytest.mark.django_db
+def test_user_patch_product(api_client, get_or_create_user_token, product_factory):
+    # arrange
+    product = product_factory()
+    user = api_client
+    user_token = get_or_create_user_token
+
+    user.credentials(HTTP_AUTHORIZATION='Token ' + user_token.key)
+    url = reverse('product-detail', args=(product.id,))
+    payload = {
+        "name": "Very Amazing Product"
+    }
+
+    # act
+    resp = api_client.patch(url, payload, format="json")
+    resp_json = resp.json()
+    expected_name = payload["name"]
+
+    # assert
+    assert resp.status_code == HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_admin_destroy_product(api_client, get_or_create_admin_token, product_factory):
+    # arrange
+    product = product_factory()
+    admin = api_client
+    admin_token = get_or_create_admin_token
+
+    admin.credentials(HTTP_AUTHORIZATION='Token ' + admin_token.key)
+    url = reverse('product-detail', args=(product.id,))
+
+    # act
+    resp = api_client.delete(url)
+
+    # assert
+    assert resp.status_code == HTTP_204_NO_CONTENT
+
+
+@pytest.mark.django_db
+def test_user_destroy_product(api_client, get_or_create_user_token, product_factory):
+    # arrange
+    product = product_factory()
+    user = api_client
+    user_token = get_or_create_user_token
+
+    user.credentials(HTTP_AUTHORIZATION='Token ' + user_token.key)
+    url = reverse('product-detail', args=(product.id,))
+
+    # act
+    resp = api_client.delete(url)
+
+    # assert
+    assert resp.status_code == HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_get_review(api_client, review_factory):
+    # arrange
+    review = review_factory()
+    url = reverse('review-detail', args=(review.id,))
+
+    # act
+    resp = api_client.get(url)
+
+    # assert
+    assert resp.status_code == HTTP_200_OK
+    assert resp.json()["id"] == review.id
+
+
+@pytest.mark.django_db
+def test_get_review_list(api_client, review_factory):
+    # arrange
+    review_1 = review_factory()
+    review_2 = review_factory()
+    url = reverse('review-list')
+
+    # act
+    resp = api_client.get(url)
+
+    # assert
+    assert resp.status_code == HTTP_200_OK
+
+    resp_json = resp.json()
+    assert len(resp_json) == 2
+
+    resp_ids = {r["id"] for r in resp_json}
+    assert resp_ids == {review_1.id, review_2.id}
+
+
+@pytest.mark.django_db
+def test_sort_reviews_by_id(api_client, review_factory):
+    # arrange
+    url = reverse('review-list')
+    review_1 = review_factory()
+    review_2 = review_factory()
+    review_3 = review_factory()
+
+    # act
+    resp = api_client.get(url)
+    sorted_reviews = {review_1.id, review_2.id, review_3.id}
+
+    # assert
+    assert resp.status_code == HTTP_200_OK
+
+    resp_json = resp.json()
+    resp_ids = {r["id"] for r in resp_json}
+    assert resp_ids == sorted_reviews
